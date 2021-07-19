cd197fde91f62f86a254514efec1db022f192f41
Camilo Barreiro <1845@holbertonschool.com>
2021-07-19 16:30:59 -0500

    Reformat file

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,10 +4,10 @@
 Exercise:
 Complex types - mixed list
 """
-from typing import Union,List
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst:List[Union[int,float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Function sum of two diferent type of number
     :param mxd_lst:
