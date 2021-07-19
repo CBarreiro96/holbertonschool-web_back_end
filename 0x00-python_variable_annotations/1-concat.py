86c3057ab327912610b39908df983a2b4d7fd375
Camilo Barreiro <1845@holbertonschool.com>
2021-07-19 13:00:18 -0500

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Exercise:
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function return sum of two string"""
+    return str1 + str2
