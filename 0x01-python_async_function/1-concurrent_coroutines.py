d6a71667a7db4137156a84be4050de3638b59790
Camilo Barreiro <1845@holbertonschool.com>
2021-07-21 19:49:08 -0500

    Clean library

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,6 @@ Exercise:
 Let's execute multiple coroutines at the same time with async
 """
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
