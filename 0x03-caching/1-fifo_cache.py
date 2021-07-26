0dd9c5dc050da6b37e4c94905dd686fad4419f5d
Camilo Barreiro <1845@holbertonschool.com>
2021-07-26 13:10:44 -0500

    FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+Exercise:
+FIFO caching
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.queque_lists = []
+
+    def put(self, key, item):
+        """ Puts item in cache """
+        if key is not None and item is not None:
+            if key not in self.queque_lists:
+                self.queque_lists.append(key)
+            else:
+                self.mv_last_list(key)
+
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first = self.get_first_list(self.queque_lists)
+                if first:
+                    self.queque_lists.pop(0)
+                    del self.cache_data[first]
+                    print("DISCARD: {}".format(first))
+
+    def mv_last_list(self, item):
+        """ Moves element to last idx of list """
+        length = len(self.queque_lists)
+        if self.queque_lists[length - 1] != item:
+            self.queque_lists.remove(item)
+            self.queque_lists.append(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """ Get first element of list or None """
+        return array[0] if array else None
+
+
